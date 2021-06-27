995899dd28ddaecd10b32bbae9c62396fb154378
RAVEEN KUMAR <63842813+raveen-2003@users.noreply.github.com>
2021-06-27 20:36:07 +0530

    Add files via upload

---
+++ covid_mark+2.py
@@ -0,0 +1,76 @@
+#covid batch +2 mark calculator
+
+print('''
+
+< covid batch +2 mark calculator >
+ --------------------------------
+        \   ^__^
+         \  (oo)\_______
+            (__)\       )\/\
+                ||----w |
+                ||     ||
+
+
+
+
+
+''')
+
+print("this is made by @im_raveen")
+print('''
+
+
+
+''')
+print("sum of top 3 marks in sslc")
+print("")
+print("")
+a = int(input("enter your tenth mark :"))
+#50 percent in sslc
+print("")
+
+print("your +1 mark out of 600")
+print("")
+b = int(input("enter your +1 mark:"))
+print("")
+print("")
+print("your +2 internal marks given by your school maximum 30% ")
+print("")
+
+c = int(input("your +2 mark (internal):"))
+
+def sslc(value):
+   print(a//3*0.5)
+   return a//3*0.5
+
+def hsc(value):
+    print(b*0.2//6)
+    return b*0.2//6	
+
+def plustwo(value):
+    print(c)
+    return c
+
+option = str(input("are you sure to calculate (yes/no):"))
+
+if option == 'yes':
+   d = sslc(a)
+
+if option == 'yes':
+    e = hsc(b)
+
+if option == 'yes':
+    f = plustwo(c)
+    
+
+print('''
+
+
+
+''')
+print("your mark in +2:")
+print(d+e+f)
+ 
+
+print("this is not accurate results , result may vary ")
+
